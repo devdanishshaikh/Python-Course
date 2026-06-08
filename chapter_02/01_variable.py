a9231ffc9020ef3566007a512b9416bb6dda65c7
devdanishshaikh <shaikhdani710@gmail.com>
2026-06-09 00:07:27 +0500

    Variable topic completed

---
+++ chapter_02/01_variable.py
@@ -0,0 +1,7 @@
+a = 1 #a is a variable that holds the value 1
+b = 2 #b is a variable that holds the value 2
+print("value of a:", a) #prints the value of a
+print("value of b:", b) #prints the value of b
+
+c = a + b #c is a variable that holds the value of a + b
+print("value of c:", c) #prints the value of c
---
+++ chapter_02/02_datatype.py
@@ -0,0 +1,9 @@
+a = 1 # a is int data type that holds the value 1
+
+b = 2.5 # b is float data type that holds the value 2.5
+
+c = "Hello, World!" # c is string data type that holds the value "Hello, World!"
+
+d = True # d is boolean data type that holds the value True
+
+e = None # e is NoneType data type that holds the value None
