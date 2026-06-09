e57cba537f1b5b87c1780479e620b3b74032c963
devdanishshaikh <shaikhdani710@gmail.com>
2026-06-09 14:40:22 +0500

    Operator concept completely covered

---
+++ chapter_02/03_operator.py
@@ -0,0 +1,32 @@
+# AssignmentOperators
+print("Arithmetic Operators:")
+a = 10
+b = 5 
+c = a+b  #addition
+d= a-b #subtraction
+e = a*b #multiplication
+f = a/b #division
+print("Sum :", c)
+print("Difference :", d)
+print("Product :", e)
+print("Quotient :", f)
+
+#assignment operators
+print("\nAssignment Operators:")
+x = 10
+print("First value:", x)
+x += 5  # equivalent to x = x + 5
+print("After addition assignment (x += 5):", x)
+
+#comparison operators
+print("\nComparison Operator")
+a = 20
+b = 25
+print("value of a :",a)
+print("Value of b :",b)
+print("Equals to a==b :",a==b)
+print("Not Equals to a!=b :",a!=b)
+print("Greater than a > b :",a>b)
+print("less than a < b :",a<b)
+print("Greater than equals to a >= b:",a>=b)
+print("less than equals to a <= b :",a<=b)
