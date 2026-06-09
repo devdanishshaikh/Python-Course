0c4ba43b3032cad2c86843b873cce19bd564b024
devdanishshaikh <shaikhdani710@gmail.com>
2026-06-09 16:51:44 +0500

    Type() function and Type casting topic covered

---
+++ chapter_02/04_typefunction.py
@@ -0,0 +1,18 @@
+a = 10
+b = "20"
+c = 3.14
+# Check type of each
+print("Before Data type conversion")
+print("Type of a:", type(a))
+print("type of b :", type(b))
+print("Type of c :", type(c))
+
+# Type casting
+print("After Data type conversion")
+a = str(a)
+b = int(b)
+c = int(c)
+
+print("Type of a:", type(a))
+print("type of b :", type(b))
+print("Type of c :", type(c))
