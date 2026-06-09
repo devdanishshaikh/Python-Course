1f17bfa0af1844d3f40a3f3af565095679ae3300
devdanishshaikh <shaikhdani710@gmail.com>
2026-06-09 17:03:37 +0500

    input() function topic covered

---
+++ chapter_02/05_inputfunction.py
@@ -0,0 +1,10 @@
+a = float(input("Enter a first  number :"))
+b = float(input("Enter a second number"))
+print("You entered a :",a)
+print("You entered b :",b)
+sum = a + b
+subtraction = a - b
+product = a*b
+print("Sum :",sum)
+print("Subtraction :",subtraction)
+print("Product :",product)
