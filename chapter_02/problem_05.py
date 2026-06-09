9fbcb1a6d2ca71eb472623f976f018abe9da1123
devdanishshaikh <shaikhdani710@gmail.com>
2026-06-09 18:01:40 +0500

    completed task 4

---
+++ chapter_02/problem_05.py
@@ -0,0 +1,5 @@
+# find average of two numbers
+num_1 = float(input("enter a first number"))
+num_2 = float(input("Enter a second number"))
+average = (num_1 + num_2)/2
+print("Average of num1 and num2 :",average)
