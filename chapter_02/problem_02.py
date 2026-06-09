48b122468bd4b7e2aa43fb8d5863171e785f5a1b
devdanishshaikh <shaikhdani710@gmail.com>
2026-06-09 17:30:26 +0500

    complete second problem to find remainder of a divided by b

---
+++ chapter_02/problem_02.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+remainder = a % b
+print(f"The remainder when {a} is divided by {b} is: {remainder}")
---
+++ chapter_02/problem_03.py
@@ -0,0 +1,11 @@
+a = input("Enter a first number: ")
+b = int(input("Enter a second number: "))
+c = float(input("Enter a third number: "))
+
+print("a :",a)
+print("b :",b)
+print("c :",c)
+
+print("a is of type: ", type(a))
+print("b is of type: ", type(b))  
+print("c is of type: ", type(c))
