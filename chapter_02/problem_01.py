af0ec77f6be445267582e35bbad3ed89c00a153e
devdanishshaikh <shaikhdani710@gmail.com>
2026-06-09 17:23:17 +0500

    Complete problem 01: sum of two numbers

---
+++ chapter_02/problem_01.py
@@ -0,0 +1,7 @@
+# Python program that add two number
+a = int(input("Enter a first Number :"))
+b = int(input("Enter a seond Number :"))
+print("a :",a)
+print("b :",b)
+sum = a + b
+print("Sum :",sum)
